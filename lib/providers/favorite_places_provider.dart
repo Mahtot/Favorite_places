44ad2f101c691018aad4accd39e83bbe1bf1f6cc
mahtot <mahtotgher13@gmail.com>
2026-05-06 12:33:58 +0300

    feat: create sqflite local database to store favorite places locally on device

--- lib/providers/favorite_places_provider.dart
+++ lib/providers/favorite_places_provider.dart
@@ -3,19 +3,67 @@ import 'package:favorite_places/models/favorite_place.dart';
 import 'package:flutter_riverpod/legacy.dart';
 import 'package:path_provider/path_provider.dart' as syspaths;
 import 'package:path/path.dart' as path;
+import 'package:sqflite/sqflite.dart' as sql;
+import 'package:sqflite/sqlite_api.dart';
+
+Future<Database> _getDatabase() async {
+  final dbPath = await sql.getDatabasesPath();
+  return sql.openDatabase(
+    path.join(dbPath, 'places.db'),
+    onCreate: (db, version) {
+      return db.execute(
+        'CREATE TABLE user_places(id TEXT PRIMARY KEY, title TEXT, image TEXT, loc_lat REAL, loc_lng REAL, address TEXT)',
+      );
+    },
+    version: 1,
+  );
+}
 
 class FavoritePlacesNotifier extends StateNotifier<List<FavoritePlace>> {
   FavoritePlacesNotifier() : super([]);
+
+  void loadPlaces() async {
+    final db = await _getDatabase();
+    final data = await db.query('user_places');
+    final places = data
+        .map(
+          (row) => FavoritePlace(
+            id: row['id'] as String,
+            title: row['title'] as String,
+            image: File(row['image'] as String),
+            location: PlaceLocation(
+              latitude: row['loc_lat'] as double,
+              longitude: row['loc_lng'] as double,
+              address: row['address'] as String,
+            ),
+          ),
+        )
+        .toList();
+    state = places;
+  }
 
   void addPlace(String title, File image, PlaceLocation location) async {
     final appDir = await syspaths.getApplicationDocumentsDirectory();
     final fileName = path.basename(image.path);
     final copiedPath = await image.copy('${appDir.path}/$fileName');
 
-    state = [
-      ...state,
-      FavoritePlace(title: title, image: copiedPath, location: location),
-    ];
+    final newPlace = FavoritePlace(
+      title: title,
+      image: copiedPath,
+      location: location,
+    );
+
+    final db = await _getDatabase();
+    await db.insert('user_places', {
+      'id': newPlace.id,
+      'title': newPlace.title,
+      'image': newPlace.image.path,
+      'loc_lat': newPlace.location.latitude,
+      'loc_lng': newPlace.location.longitude,
+      'address': newPlace.location.address,
+    });
+
+    state = [...state, newPlace];
   }
 }
 
